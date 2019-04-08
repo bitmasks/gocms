4884f115c4a6eb010507626bbb5c5cd283330f29
taorong <taorong@hoolistudent.com>
2019-04-08 18:11:58 +0800

    init

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,8 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["WebsiteName"] = "网站名称"
+	c.Data["WebsiteKeywords"] = "关键词"
+	c.Data["WebsiteDescription"] = "网站描述"
 	c.TplName = "index.tpl"
 }
